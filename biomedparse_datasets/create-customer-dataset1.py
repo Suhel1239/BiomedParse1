f6cc4b89c0dddb4525df62d9d34e296bd1824be0
Suhel1239 <124263113+Suhel1239@users.noreply.github.com>
2025-09-03 19:17:25 +0530

    Update create-customer-dataset1.py

--- biomedparse_datasets/create-customer-dataset1.py
+++ biomedparse_datasets/create-customer-dataset1.py
@@ -63,7 +63,7 @@ def get_bbox_and_area(mask_path):
 
     x_min, x_max = xs.min(), xs.max()
     y_min, y_max = ys.min(), ys.max()
-    bbox = [int(x_min), int(y_min), int(x_max), int(y_max)]
+    bbox = [int(x_min), int(y_min), int(x_max - x_min), int(y_max - y_min)]
     area = int(np.sum(mask > 0))
     return bbox, area
 
