abda06e5fa3ba311be3a87e1c20c1c785d2af111
Suhel1239 <124263113+Suhel1239@users.noreply.github.com>
2025-09-08 19:12:31 +0530

    Update train.sh

--- assets/scripts/train.sh
+++ assets/scripts/train.sh
@@ -16,9 +16,9 @@ CUDA_VISIBLE_DEVICES=0 mpirun -n 1 python entry.py train \
             MODEL.DECODER.HIDDEN_DIM 512 \
             MODEL.ENCODER.CONVS_DIM 512 \
             MODEL.ENCODER.MASK_DIM 512 \
-            TEST.BATCH_SIZE_TOTAL 4 \
-            TRAIN.BATCH_SIZE_TOTAL 4 \
-            TRAIN.BATCH_SIZE_PER_GPU 4 \
+            TEST.BATCH_SIZE_TOTAL 1 \
+            TRAIN.BATCH_SIZE_TOTAL 1 \
+            TRAIN.BATCH_SIZE_PER_GPU 1 \
             SOLVER.MAX_NUM_EPOCHS 20 \
             SOLVER.BASE_LR 0.00001 \
             SOLVER.FIX_PARAM.backbone False \
